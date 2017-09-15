6699b37f81e0247b26b0842a841baaea2e846cb5
Amarth Gul <amarthgul@gmail.com>
2017-09-15 01:20:02 -0700

    hail
    Now the add() could work for integers

--- DeviantData/SixtyHex.py
+++ DeviantData/SixtyHex.py
@@ -1,48 +1,56 @@
-def timeCalcul():
-    import re
-    class babs():
-        def __init__(self, input = [0, 0, 0]):
-            self._hour = input[0] #also degree
-            self._min = input[1]
-            self._sec = input[2]
+import re
+class babs():
+    def __init__(self, input = [0, 0, 0]):
+        self.maxBit = 3
+        self._value = []
 
-        @property
-        def hour(self): return self._hour
-        @property
-        def min(self): return self._min
-        @property
-        def sec(self): return self._sec
-        @property
-        def value(self): return [self._hour, self._min, self._sec]
+    @property
+    def value(self): return self._value
 
-        def _add(self, babsOne, babsTwo):
-            return (babsOne + babsTwo) % 60, (babsOne + babsTwo) // 60
+    def _add(self, babsOne, babsTwo):
+        return (babsOne + babsTwo) % 60, (babsOne + babsTwo) // 60
 
-        def decToBabs(self, decNum): return [decNum//60, decNum%60]
-        def babsToDec(self, babsNum): 
-            if isinstance(babsNum, list): babsList = babsNum
-            if isinstance(babsNum, str):
-                babsList = re.findall(r'(\d+)', babsNum)
-                babsList = list(map(int, babsList))
-            decNum = 0
+    def decToBabs(self, decNum): return [decNum//60, decNum%60]
+    def babsToDec(self, babsNum): 
+        '''Input can either be list or str
+        But have to be in form like [12, 34, ...] for list
+        And "12 34 ..."(seperated by non-num characters) for str '''
+        if isinstance(babsNum, list): babsList = babsNum
+        if isinstance(babsNum, str):
+            babsList = re.findall(r'(\d+)', babsNum)
+            babsList = list(map(int, babsList))
+        decNum = 0
+        for i in range(len(babsList)-1, 0, -1):
+            if babsList[i] >= 60: raise ValueError
+            decNum = (babsList[i]+decNum) / 60
+        decNum += babsList[0]
+        return decNum
 
-            for i in range(len(babsList)-1, 0, -1):
-                if babsList[i] >= 60: raise ValueError
-                decNum = (babsList[i]+decNum) / 60
-            decNum += babsList[0]
-            return decNum
+    def add(self, *args):
+        out = [0]
 
-        def add(self, *args):
-            scaler = []; babsNum = []; 
+        # 1.) classify data input:
+        listNum = []; strNum = []; decNum = 0 
+        for var in args:
+            if isinstance(var, list): listNum.append(var)
+            elif isinstance(var, str): strNum = var
+            elif isinstance(var, int): decNum += var
+            else: raise ValueError
+        # 2.) modify str data:
+        if strNum:
+            strNum = eval(strNum)
+            if isinstance(strNum, list): listNum.extend(var)
+            elif isinstance(strNum, int): decNum += var
+            elif isinstance(strNum, tuple): decNum += sum(strNum)
+            else: raise ValueError
 
-            for var in args:
-                if isinstance(var, list): babsNum.append(var)
-                #elif isinstance(var, str): single = var
-                else: scaler.append(var)
+        #print(listNum, strNum, decNum)
 
-            print(babsNum, scaler)
+        while True:
+            higher, lower = self.decToBabs(decNum)
+            out[0] = lower; out.insert(0, higher)
+            if len(out) >= self.maxBit:
+                break
+            decNum = higher
 
-    a = babs()
-    #a.add([12, 23, 34], 45)
-    print(a.babsToDec('12, 34, 50'))
-    return 0
+        #print(out)
