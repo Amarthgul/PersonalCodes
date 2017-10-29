a4b91577b96a18a66e9513d985e68cd1e120c7ec
Amarth Gul <amarthgul@gmail.com>
2017-10-29 08:16:35 -0700

    deleted redundant

--- phyData/expansionIndex.py
+++ phyData/expansionIndex.py
@@ -2,69 +2,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-class dataPlot():
-    def __init__(self):
-        self.xData = []
-        self.yData = []
-        self.xLabel = 'X'
-        self.yLabel = 'Y'
-        self.type = 'o' 
-        self.mainPlotColor = 'red'
-        self.gridColor = 'r'
-        self.alp = 0.5
-        self.biStrip = True
-        self.xStripMaj = 1
-        self.xStripMin = 1
-        self.yStripMaj = 0.1
-        self.yStripMin = 0.1
-        self.xStripMajWid = 1
-        self.xStripMinWid = 1
-        self.yStripMajWid = 0.1
-        self.yStripMinWid = 0.1
-        self.fig = plt.figure()
-        self.ax = plt.axes()
-		
-    def creatPlot(self, *args):
-        if len(self.xData) and len(self.yData):
-            self.ax.plot(self.xData, self.yData, *args)
-
-    def addPlot(self, x, y, plotType = None, addLabel = 'Sample'):
-        if not plotType:
-            ax.plot(x, y, addLabel)
-        else:
-            ax.plot(x, y, plotType, addLabel)
-
-    def addGrid(self):
-        self.ax.xaxis.set_major_locator(plt.MultipleLocator(self.xStripMaj))
-        self.ax.yaxis.set_major_locator(plt.MultipleLocator(self.yStripMaj))
-        self.ax.grid(which='major', axis='x', linewidth=self.xStripMajWid, 
-                linestyle='-', color = self.gridColor, alpha = self.alp)
-        self.ax.grid(which='major', axis='y', linewidth=self.yStripMajWid, 
-                linestyle='-', color = self.gridColor, alpha = self.alp)
-                
-        if self.biStrip:
-        	self.ax.yaxis.set_minor_locator(plt.MultipleLocator(self.yStripMin))
-        	self.ax.xaxis.set_minor_locator(plt.MultipleLocator(self.xStripMin))
-        	self.ax.grid(which='minor', axis='y', linewidth=self.yStripMinWid, 
-                linestyle='-', color = self.gridColor, alpha = self.alp)
-        	self.ax.grid(which='minor', axis='x', linewidth=self.xStripMinWid, 
-                linestyle='-', color = self.gridColor, alpha = self.alp)
-
-    def showPlot(self):
-        plt.show()
-
-
-def plotSam():
-    fig = dataPlot()
-    fig.xData = np.array([1, 2, 3, 4])
-    fig.yData = np.array([2.5, 3.3, 4.1, 5.6])
-    fig.creatPlot()
-    fig.addGrid()
-    fig.showPlot()
-
-
-plotSam()
-
 def phy17_9_25_():
     grid = True; gridColor = 'r'; alp = 0.5; scalar = 1.5
     deltaTemp = np.array([0, 5, 10, 15, 20, 25, 30])
