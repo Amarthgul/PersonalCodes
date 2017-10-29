7ae5302b04cab86350732a0e3334014a537062b2
Amarth Gul <amarthgul@gmail.com>
2017-10-29 12:37:05 +0800

    Update classPlot.py

--- phyData/classPlot.py
+++ phyData/classPlot.py
@@ -3,6 +3,7 @@ import numpy as np
 
 class dataPlot():
     def __init__(self):
+        self.ovaScale = 1
         self.xData = []
         self.yData = []
         self.xLabel = 'X'
@@ -22,11 +23,12 @@ class dataPlot():
         self.yStripMaj = 1
         self.yStripMin = 0.1
         self.xStripMajWid = 1
-        self.xStripMinWid = 0.25
+        self.xStripMinWid = 0.5
         self.yStripMajWid = 1
-        self.yStripMinWid = 0.25
+        self.yStripMinWid = 0.5
         
-        self.fig = plt.figure(self.figureSize)
+        
+        #self.fig = plt.figure(figsize = self.figureSize)
         self.ax = plt.axes() if not self.axesPara else plt.axes(self.axesPara)
 		
     def creatPlot(self, *args):
@@ -36,21 +38,26 @@ class dataPlot():
     def addGrid(self):
         self.ax.xaxis.set_major_locator(plt.MultipleLocator(self.xStripMaj))
         self.ax.yaxis.set_major_locator(plt.MultipleLocator(self.yStripMaj))
-        self.ax.grid(which='major', axis='x', linewidth=self.xStripMajWid, 
+        self.ax.grid(which='major', axis='x', linewidth=self.xStripMajWid * self.ovaScale, 
                 linestyle='-', color = self.gridColor, alpha = self.alp)
-        self.ax.grid(which='major', axis='y', linewidth=self.yStripMajWid, 
+        self.ax.grid(which='major', axis='y', linewidth=self.yStripMajWid * self.ovaScale, 
                 linestyle='-', color = self.gridColor, alpha = self.alp)       
         if self.biStrip:
             self.ax.yaxis.set_minor_locator(plt.MultipleLocator(self.yStripMin))
             self.ax.xaxis.set_minor_locator(plt.MultipleLocator(self.xStripMin))
-            self.ax.grid(which='minor', axis='y', linewidth=self.yStripMinWid, 
-                linestyle='-', color = self.gridColor, alpha = self.alp)
-            self.ax.grid(which='minor', axis='x', linewidth=self.xStripMinWid, 
-                linestyle='-', color = self.gridColor, alpha = self.alp)
+            self.ax.grid(which='minor', axis='y', 
+                         linewidth=self.yStripMinWid * self.ovaScale, 
+                         linestyle='-', color = self.gridColor, alpha = self.alp)
+            self.ax.grid(which='minor', axis='x', 
+                         linewidth=self.xStripMinWid * self.ovaScale, 
+                         linestyle='-', color = self.gridColor, alpha = self.alp)
         
     def showPlot(self):
-        plt.xlabel(self.xLabel, fontsize = self.labelFontSize)
-        plt.ylabel(self.yLabel, fontsize = self.labelFontSize)
+        if self.figureSize != None:
+            plt.rcParams["figure.figsize"] = (self.figureSize[0]*self.ovaScale,
+                self.figureSize[1]*self.ovaScale)
+        plt.xlabel(self.xLabel, fontsize = self.labelFontSize*self.ovaScale)
+        plt.ylabel(self.yLabel, fontsize = self.labelFontSize*self.ovaScale)
         if self.limBorder:
             plt.xlim(self.xLim[0], self.xLim[1])
             plt.ylim(self.yLim[0], self.yLim[1])
