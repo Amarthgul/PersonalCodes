adf185dbaf95b66505d6dffdf8b8a49032f37ffb
Amarth Gul <amarthgul@gmail.com>
2018-01-08 18:26:54 -0700

    Update carriageSimulator.c

--- Sharing/carriageSimulator/carriageSimulator.c
+++ Sharing/carriageSimulator/carriageSimulator.c
@@ -1,9 +1,9 @@
 #include "reg52.h"
-#define UINTLIM 500
-#include <intrins.h>
+#include <string.h>
 #ifndef __cplusplus
 typedef enum { False = 0, True = !False } bool;
 #endif
+#define UINTLIM 500
 #define TUBE P0
 
 typedef unsigned int u16; //Seriously I don't want to use these abbr
@@ -27,8 +27,10 @@ sbit motorFowButton = P2 ^ 1; //Deluxe button
 sbit motorRevButton = P2 ^ 0; //Deluxe button
 
 unsigned int second = 10;
-unsigned char shapeFow[] = {0x00, 0x00, 0x00, 0x00, 0x40, 0x40, 0x40, 0x70};
-unsigned char shapeRev[] = {0x46, 0x40, 0x40, 0x40, 0x00, 0x00, 0x00, 0x00};
+unsigned char digit[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f,
+	0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71}; // 1, 2, 3 ... D, E, F
+unsigned char shapeFow[] = {0x00, 0x40, 0x40, 0x70, 0x00, 0x40, 0x40, 0x70};
+unsigned char shapeRev[] = {0x46, 0x40, 0x40, 0x00, 0x46, 0x40, 0x40, 0x00};
 unsigned char shapeHalt[] = {0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40};
 char motorDirection = 0;
 
@@ -76,7 +78,7 @@ void digDisplay(unsigned char * diplayArray, unsigned int delayTime) {
 	}
 }
 void digForwarding(){
-	/* Shift the shapeFow array to left */
+	/* Shift the shapeFow array right */
 	char temp = shapeFow[0];
 	int iterator;
 	shapeFow[0] = shapeFow[7];
@@ -85,7 +87,7 @@ void digForwarding(){
 	shapeFow[1] = temp;
 }
 void digbackwarding(){
-	/* Shift the shapeFow array to left */
+	/* Shift the shapeRev array left */
 	char temp = shapeRev[7];
 	int iterator;
 	shapeRev[7] = shapeRev[0];
@@ -95,7 +97,7 @@ void digbackwarding(){
 }
 void delaySecond(int time_Second) {
 	/*Delay the time as the input, unity: second
-	Usually have 1 second's err*/
+	Usually have 1 second's err, and deprives evething else*/
 	second = time_Second + 1;
 	while (True){
 		if (second < 1){
@@ -115,12 +117,12 @@ int scanButton(button) {
 }
 void motorForward(bool forward, unsigned int argSpeed){
 	/* Should work either in loop or simply switch direction */
-	unsigned int delayTime = 65500 - argSpeed;
+	unsigned int delayTime = 65535 - argSpeed;
 	if (forward) {
-		motorFow = 1; motorRev = 0; motorDirection = 1;
+		motorFow = 1; motorRev = ~motorFow; motorDirection = 1;
 	}
 	else {
-		motorFow = 0; motorRev = 1; motorDirection = -1;
+		motorFow = 0; motorRev = ~motorFow; motorDirection = -1;
 	}
 	delay(delayTime);
 }
@@ -129,82 +131,81 @@ void motorHalt(){
 	motorDirection = 0;
 }
 
-void demoMode(){
-	/* Testing */
-	unsigned int delayTime, step, upperBound,lowerBound;
-	delayTime = (int)(UINTLIM / 2); step = 50;
-	upperBound = UINTLIM; lowerBound = 10;
-	while (True){
-
-		if (scanButton(accelerateButton))
-			delayTime -= step;
-		if (scanButton(decreaseButton))
-			delayTime += step;
-		if (scanButton(motorFowButton)) {
-			motorForward(True, delayTime);
-			motorDirection = 1;
-		}
-		if (scanButton(motorRevButton)) {
-			motorForward(False, delayTime);
-			motorDirection = -1;
-		}
-
-		motorHalt();
-		delay(100);
-		if (motorDirection) {
-			motorForward(True, delayTime); }
-		else if (motorDirection == -1){
-			motorForward(False, delayTime);}
-
-
-		if (delayTime > upperBound) delayTime = upperBound;
-		if (delayTime < lowerBound) delayTime = lowerBound;
-	}
-}
 
 void classMode(){
 	/* As the assignment asked */
-	while(! scanButton(startButton))
-		digDisplay(shapeHalt, 10);
-	while(! scanButton(buttonSQ2)){
-		motorForward(True, 65250);
-		if (second <= 9){
+	bool digFlag = False;
+	unsigned char digDup[8];
+	//=====================Waiting startButton========================
+	memcpy(digDup, shapeHalt, sizeof(shapeHalt));
+	digDup[4] = digDup[5] = digDup[6] = digDup[7] = 0;
+	while(! scanButton(startButton)) {
+		if (second <= 9) {
+			digFlag = ~digFlag; second = 10; }
+		if (digFlag) {digDup[4] = 0x00; }
+		else {digDup[4] = digit[5]; }
+		digDisplay(digDup, 1);
+	}
+	//==============startButton Pressed, waiting buttonSQ2==========
+	while(! scanButton(buttonSQ2)) {
+		memcpy(digDup, shapeFow, sizeof(shapeFow));
+		digDup[4] = digit[5]; digDup[6] = digit[2];
+		digDup[5] = digDup[7] = 0;
+		motorForward(True, 65500);
+		if (second <= 9) {
 			digForwarding();
-			second = 10;
-		}
-		digDisplay(shapeFow, 10);
+			second = 10; digFlag = ~digFlag; }
+		if (digFlag) {digDup[6] = 0; }
+		else {digDup[6] = digit[2]; }
+		digDisplay(digDup, 1);
 	}
-	while(! scanButton(buttonSQ1)){
-		motorForward(False, 65250);
-		if (second <= 9){
+	//==============buttonSQ2 Pressed, waiting buttonSQ1==========
+	while(! scanButton(buttonSQ1)) {
+		memcpy(digDup, shapeRev, sizeof(shapeRev));
+		digDup[4] = digDup[7] = 0;
+		digDup[5] = digit[1]; digDup[6] = digit[2];
+		motorForward(False, 65500);
+		if (second <= 9) {
 			digbackwarding();
-			second = 10;
-		}
-		digDisplay(shapeRev, 10);
+			second = 10; digFlag = ~ digFlag; }
+		if (digFlag) {digDup[5] = 0; }
+		else {digDup[5] = digit[1]; }
+		digDisplay(digDup, 1);
 	}
+	//==============buttonSQ1 Pressed, waiting buttonSQ3==========
 	motorHalt();
 	second = 10;
-	while(second > 1)
-		digDisplay(shapeHalt, 10);
-	while(! scanButton(buttonSQ3)){
-		motorForward(True, 65250);
-		if (second <= 9){
-			digForwarding();
-			second = 10;
-		}
-		digDisplay(shapeFow, 10);
+	while(second > 1){
+		digDisplay(shapeHalt, 1);
 	}
+	while(! scanButton(buttonSQ3)){
+		memcpy(digDup, shapeFow, sizeof(shapeFow));
+		digDup[4] = digDup[6] = 0;
+		digDup[5] = digit[1]; digDup[7] = digit[3];
+		motorForward(True, 65500);
+		if (second <= 9) {
+			digForwarding();
+			second = 10; digFlag = ~digFlag; }
+		if (digFlag) {digDup[7] = 0; }
+		else {digDup[7] = digit[3]; }
+		digDisplay(digDup, 1);
+	}
+	//==============buttonSQ3 Pressed, waiting buttonSQ1==========
 	while(! scanButton(buttonSQ1)){
-		motorForward(False, 65250);
-		if (second <= 9){
+		memcpy(digDup, shapeRev, sizeof(shapeRev));
+		digDup[4] = digDup[6] = 0; digDup[5] = digit[1]; digDup[7] = digit[3];
+		motorForward(False, 65500);
+		if (second <= 9) {
 			digbackwarding();
-			second = 10;
-		}
-		digDisplay(shapeRev, 10);
+			second = 10; digFlag = ~ digFlag; }
+		if (digFlag) {digDup[5] = 0; }
+		else {digDup[5] = digit[1]; }
+		digDisplay(digDup, 1);
 	}
 	motorHalt();
-	while(! scanButton(startButton))
-		digDisplay(shapeHalt, 10);
+	while(! scanButton(startButton)){
+		digDisplay(shapeHalt, 1);
+	}
 }
 
 void main() {
