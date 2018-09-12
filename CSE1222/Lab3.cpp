f53399a2bca1501325eb2c430bb4430080bacd14
Amarth Gul <amarthgul@gmail.com>
2018-09-12 18:42:07 -0400

    Update Lab3.cpp

--- CSE1222/Lab3.cpp
+++ CSE1222/Lab3.cpp
@@ -1,5 +1,5 @@
 // File: investments.cpp
-// Created by: Guoping CHen (chen.8759)
+// Created by: Anon
 // Created on: 12 Sept, 2018
 
 /* (: my comment here :) */
@@ -22,40 +22,7 @@ int main(){
 	double profitCombo;
 	
 	
-	/* get user input */
-	cout << "Enter initial investment (dollars): ";
-	cin >> initInvest;
-	cout << "Enter interest rate per year (percentage): ";
-	cin >> interestRate;
-	cout << "Enter number of months: ";
-	cin >> numMonth;
-
-
-	/* Calculation */
-	numYear		= numMonth / 12.0;
-	//The good old way
-	finalValue	= initInvest * pow(1 + (interestRate / 100.0), numYear);
-	profit		= finalValue - initInvest;
-	// Combo!!!
-	finalValueCombo = initInvest * pow(EULER, (interestRate / 100.0) * numYear);
-	profitCombo		= finalValueCombo - initInvest;
-
-
-	/*Output result*/
-	cout << endl; //The good old way
-	cout << "Value of your investment compounded annually after " << numYear 
-		<< " year(s) is " << finalValue << " dollars." << endl;
-	cout << "Profit from your investment after " << numYear 
-		<< " year(s) is " << profit << " dollars." << endl << endl;
-	// Combo!!!
-	cout << "Value of your investment continuously compounded after " 
-		<< numYear << " year(s) is " << finalValueCombo 
-		<< " dollars." << endl;
-	cout << "Profit from this investment is " << profitCombo
-		<< " dollars." << endl << endl;
-	// Compare
-	cout << "The difference between both investment types is " 
-		<< abs(profit - profitCombo) << " dollars." << endl;
+	/* code deleted */
 
 	return (0);
 }
