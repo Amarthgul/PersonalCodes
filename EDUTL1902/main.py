cb2171b8decdf00d79904c47380f928061af3d5f
Amarth Gul <amarthgul@gmail.com>
2018-09-15 22:35:44 -0400

    Update main.py

--- EDUTL1902/main.py
+++ EDUTL1902/main.py
@@ -2,18 +2,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-
-dataPath = r'C:\Amarth\Documents\OSU Assignments\EDUTL 1902\dataSet.pckl'
 xlsPath = r'C:\Amarth\Documents\OSU Assignments\EDUTL 1902\Data.xls'
 possibleType = ["wyvern", "european", "asian", "multiple", 
-                "other", "mixture", "drake", "multi-head"]
+                "other", "mixture", "drake", "amphiptere"]
+categDict = {"wyvern":      ["wyvern"],
+                "european":    ["european"],
+                "asian":       ["asian", "serpentine"],
+                "other":       ["multiple", "other", "mixture"],
+                "drake":       ["drake"],
+                "amphiptere" : ["amphiptere"]}
 dataSet = pd.read_excel(xlsPath, encoding = "ISO-8859-1")
 
-def cleanType(dragonType):
+def cleanType(dragonType, combineMultOth = True):
+
     dragonType = str(dragonType).lower()
-    for instance in possibleType:
-        if instance in dragonType:
-            return instance
+    #print(dragonType, end = " ")
+    for mainTag in categDict:
+        
+        for subTag in categDict[mainTag]:
+            if subTag in dragonType:
+                #print("classified as ", mainTag)
+                return mainTag
+    
 
 def typeWithYear():
     ''' Type of dragon in every year film 
@@ -22,27 +32,48 @@ def typeWithYear():
     '''
     relation = {}
     for rowIndex in range(len(dataSet)):
-        currentItemReleaseYear = dataSet["YearReleased"][rowIndex]
+        currentItemReleaseYear = dataSet["YearReleased"][rowIndex] - dataSet["YearReleased"][rowIndex]%5
         if np.isnan(currentItemReleaseYear):
             continue
         if currentItemReleaseYear not in relation:
             relation[int(currentItemReleaseYear)] = {}
+        #print(rowIndex, end = '  ')
         currentDragonType = cleanType(dataSet["DragonType"][rowIndex])
         if currentDragonType not in relation[int(currentItemReleaseYear)]:
             relation[int(currentItemReleaseYear)][currentDragonType] = 1
         else:
             relation[int(currentItemReleaseYear)][currentDragonType] += 1
 
-    for year in relation: #modifi into same dict items
-        for instance in possibleType:
+    for year in relation: #unify
+        for instance in categDict:
             if instance not in relation[year]:
                 relation[year][instance] = 0
 
-    print(relation)
+    
+    return relation
+def plotTypeWithYear():
+    df = pd.DataFrame(typeWithYear())
+    df = df.T
+    print(df)
+    
+    plt.style.use(u'ggplot')
+    df.fillna(df).astype(df.dtypes).plot.bar(stacked=True)
+    plt.show()
+
+    return 0
+
+def plotTypeWithYearMean():
+    df = pd.DataFrame(typeWithYear())
+    print(df)
+    
+    plt.style.use(u'ggplot')
+    df.fillna(df).astype(df.dtypes).plot.bar(stacked=True)
+    plt.show()
+    return 0
     
 
 def main():
-    typeWithYear()
+    plotTypeWithYearMean()
 
 if __name__ == '__main__':
     main()
