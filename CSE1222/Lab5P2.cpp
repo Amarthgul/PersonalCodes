ded4978f5eeacb231d91e06589a88e1ac0d929b5
Amarth Gul <amarthgul@gmail.com>
2018-09-27 11:49:51 -0400

    Update and rename Lab5P2 to Lab5P2.cpp

---
+++ CSE1222/Lab5P2.cpp
@@ -0,0 +1,16 @@
+/*
+File:          triangle.cpp
+Created by:    Anon
+Creation Date: 27 Sept, 2018
+Synopsis: print a triangle shape made of digits
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//code deleted
+}
+
+//thank goodnedd c++ can declare variable out of the top of `main()`
