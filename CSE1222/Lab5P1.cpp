0f9c37ad7ebb7da51f6ef5a7f5f7a0a486020722
Amarth Gul <amarthgul@gmail.com>
2018-09-27 11:23:42 -0400

    Update Lab5P1.cpp

--- CSE1222/Lab5P1.cpp
+++ CSE1222/Lab5P1.cpp
@@ -19,42 +19,37 @@ int main()
 	int numF(0);
 	int numK(0);
 	int numJ(0);
-	string userInput;
+	char currentStr;
 
 	cout << "Enter text :" << endl;
 	while (true) { // avoid code duplication
-		cin >> userInput;
+		cin >> currentStr;
 
-		int i(0); //iterator
-		while (i < userInput.size()) {
-			if (userInput.at(i) == '.' || userInput.at(i) == '!') { 
-				//end condition
-				cout << "\nNumber of a's (worth 1 point each) : " << numA << endl;
-				cout << "Number of g's (worth 2 points each) : " << numG << endl;
-				cout << "Number of m's (worth 3 points each) : " << numM << endl;
-				cout << "Number of f's (worth 4 points each) : " << numF << endl;
-				cout << "Number of k's (worth 5 points each) : " << numK << endl;
-				cout << "Number of j's (worth 8 points each) : " << numJ << endl << endl;
-				cout << "Total score: "
-					<< numA + numG * 2 + numM * 3 + numF * 4 + numK * 5 + numJ * 8;
-				//the solution demo given does not have an endline
+		if (currentStr  == '.' || currentStr  == '!') { 
+			//end condition
+			cout << "\nNumber of a's (worth 1 point each) : " << numA << endl;
+			cout << "Number of g's (worth 2 points each) : " << numG << endl;
+			cout << "Number of m's (worth 3 points each) : " << numM << endl;
+			cout << "Number of f's (worth 4 points each) : " << numF << endl;
+			cout << "Number of k's (worth 5 points each) : " << numK << endl;
+			cout << "Number of j's (worth 8 points each) : " << numJ << endl << endl;
+			cout << "Total score: "
+				<< numA + numG * 2 + numM * 3 + numF * 4 + numK * 5 + numJ * 8;
+			//the solution demo given does not have an endline
 
-				return 0; //exit here instead of at the bottom of `main()`
-			}
-			else if (userInput.at(i) == 'a' || userInput.at(i) == 'A')
-				numA ++;
-			else if (userInput.at(i) == 'g' || userInput.at(i) == 'G')
-				numG ++;
-			else if (userInput.at(i) == 'm' || userInput.at(i) == 'M')
-				numM ++;
-			else if (userInput.at(i) == 'f' || userInput.at(i) == 'F')
-				numF ++;
-			else if (userInput.at(i) == 'k' || userInput.at(i) == 'K')
-				numK ++;
-			else if (userInput.at(i) == 'j' || userInput.at(i) == 'J')
-				numJ ++;
-
-			i ++; 
+			return 0; //exit here instead of at the bottom of `main()`
 		}
+		else if (currentStr == 'a' || currentStr == 'A')
+			numA ++;
+		else if (currentStr == 'g' || currentStr == 'G')
+			numG ++;
+		else if (currentStr == 'm' || currentStr == 'M')
+			numM ++;
+		else if (currentStr == 'f' || currentStr == 'F')
+			numF ++;
+		else if (currentStr == 'k' || currentStr == 'K')
+			numK ++;
+		else if (currentStr == 'j' || currentStr == 'J')
+			numJ ++;
 	}
 }
