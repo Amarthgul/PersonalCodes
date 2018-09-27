e22cdf6448833b0437bced5a4e7179dd5bd9b3f5
Amarth Gul <amarthgul@gmail.com>
2018-09-27 11:12:48 -0400

    Update Lab5P1.cpp

--- CSE1222/Lab5P1.cpp
+++ CSE1222/Lab5P1.cpp
@@ -1,12 +1,13 @@
 /*
 File:          scrabble.cpp
 Created by:    Guoping Chen (Chen.8759)
-Creation Date: 22 Sept, 2018
-Synopsis: Compare the position of 2 points
+Creation Date: 27 Sept, 2018
+Synopsis: count occurrences of certain letters 
 */
 
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
 int main()
@@ -21,22 +22,24 @@ int main()
 	string userInput;
 
 	cout << "Enter text :" << endl;
-	while (true) {
+	while (true) { // avoid code duplication
 		cin >> userInput;
 
-		int i(0);
+		int i(0); //iterator
 		while (i < userInput.size()) {
-			if (userInput.at(i) == '.' || userInput.at(i) == '!') {
+			if (userInput.at(i) == '.' || userInput.at(i) == '!') { 
+				//end condition
 				cout << "\nNumber of a's (worth 1 point each) : " << numA << endl;
 				cout << "Number of g's (worth 2 points each) : " << numG << endl;
 				cout << "Number of m's (worth 3 points each) : " << numM << endl;
 				cout << "Number of f's (worth 4 points each) : " << numF << endl;
 				cout << "Number of k's (worth 5 points each) : " << numK << endl;
 				cout << "Number of j's (worth 8 points each) : " << numJ << endl << endl;
-
 				cout << "Total score: "
 					<< numA + numG * 2 + numM * 3 + numF * 4 + numK * 5 + numJ * 8;
-				return 0;
+				//the solution demo given does not have an endline
+
+				return 0; //exit here instead of at the bottom of `main()`
 			}
 			else if (userInput.at(i) == 'a' || userInput.at(i) == 'A')
 				numA ++;
@@ -51,7 +54,7 @@ int main()
 			else if (userInput.at(i) == 'j' || userInput.at(i) == 'J')
 				numJ ++;
 
-			i++;
+			i ++; 
 		}
 	}
 }
