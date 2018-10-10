6141f25bb7c458d80e9c38cb9eb0f0d9fcee1df5
Amarth Gul <amarthgul@gmail.com>
2018-10-09 20:09:10 -0400

    Update Lab6.cpp

--- CSE1222/Lab6.cpp
+++ CSE1222/Lab6.cpp
@@ -1,6 +1,6 @@
 /*
 File:          alt_harmonic.cpp
-Created by:    Guoping Chen (Chen.8759)
+Created by:    anon
 Creation Date: 9 Oct, 2018
 Synopsis:      calculate harmonic number
 */
@@ -11,28 +11,8 @@ using namespace std;
 
 int main()
 {
-	/* variable declaration */
-	int userNum;
-	double harmonic(0);
 
-
-	/* get and verify user input */
-	cout << "Enter n: ";
-	cin >> userNum;
-	while (userNum <= 1) {
-		cout << "Value n must be 1 or greater. Try again: ";
-		cin >> userNum;
-	}
-
-
-	/* calculation */
-	for (int k = 1; k <= userNum; k++) {
-		harmonic += pow(-1, k + 1) / k;
-	}
-
-
-	/* display result */
-	cout << "The alternating series converges to " << harmonic << endl;
-
+	// code deleted
+	
 	return 0;
 }
