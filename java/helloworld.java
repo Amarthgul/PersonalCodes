6041b730ac7219a69211ab5d4165cd7b99d34e55
Amarth Gul <amarthgul@gmail.com>
2018-12-21 18:32:26 -0500

    Create helloworld.java

---
+++ java/helloworld.java
@@ -0,0 +1,8 @@
+package helloWorld;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Fuck the world");
+	}
+} // so the comment is the same with C :thinking:
