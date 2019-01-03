f3c7afb7c6c1f6f623d34c843d3e7ade62b12589
Amarth Gul <amarthgul@gmail.com>
2019-01-03 17:22:55 -0500

    Update helloworld.java

--- java/helloworld.java
+++ java/helloworld.java
@@ -1,8 +1,27 @@
 package helloWorld;
+import java.math.*;
 
 public class HelloWorld {
 	
-	public static void main(String[] args) {
-		System.out.println("Fuck the world");
+	public static void main(String[] args) {	
+		/* Initializing */
+		int x = 0;
+		boolean dump = true;
+		byte biteMe = 1; //in between -127 ~ 128
+		BigInteger bigNum = new BigInteger("1242423231"); //big
+		double [] randList = new double [10]; //list
+		randList[0] = (double)biteMe; //casting
+		final int MARK = 3; //constant 
+		
+		/* operating */
+
+		
+		// Dump
+		if (dump) {
+			x += MARK;
+			bigNum = bigNum.multiply(bigNum);
+			
+		}
 	}
-} // so the comment is the same with C :thinking:
+
+} 
