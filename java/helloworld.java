05e46f2db6acb5866ecab9a4117781b252776679
Amarth Gul <amarthgul@gmail.com>
2019-01-04 23:22:30 -0500

    Update helloworld.java

--- java/helloworld.java
+++ java/helloworld.java
@@ -1,13 +1,18 @@
+/* This program is created to help people with other programming experiences
+ * (c++, python, etc.) get familiar with java quickly  
+*/
+
 package helloWorld;
-import java.math.*; //import all in .math
+import java.math.*; //import all classes in java.math
 import java.util.Scanner; //impoer a single class
+import java.util.Random;
 
 import javax.swing.JOptionPane;
 
 public class HelloWorld {
 	
 	public static void main(String[] args) {	
-		HelloWorld.basicIO();
+		HelloWorld.basicBranchAndLoops();
 	}
 	
 	public static void basicDatatype() {
@@ -22,14 +27,15 @@ public class HelloWorld {
 		double [] randList = new double [10]; //java array
 		BigInteger bigNum = new BigInteger("1242423231");    //large number
 		BigDecimal bigDec = new BigDecimal("3.14159265354"); //large float
+		
 	}
 	
-	public static void basicIO() {
-		int input;
+	public static void basicConsoleIO() {
 		Scanner in = new Scanner(System.in); // declare a scanner for user input	
 		System.out.print("This is static method `print`, "); // same as `cpp cout`
 		System.out.print("followed with another `print`. \n Enter an integer: "); // no auto-newline
-		input = in.nextInt(); //read from scanner
+		int input = in.nextInt(); //read from scanner
+		//Also declaration can be done aside from function/method top
 		/* other scanner methods:
 		 *    float:          SCANNER.nextDouble();
 		 *    String:         SCANNER.next();
@@ -40,9 +46,30 @@ public class HelloWorld {
 		System.out.println("All over");
 	}
 	
-	public static void callingGUIforIO(String[] args) {
+	public static void callingGUIforIO() {
 		String input = JOptionPane.showInputDialog("Enter name:"); //GUI interface
 		System.out.print("Omae Wa Mou Shindeiru! " + input + "!"); // concatenate Strings
+		// Still output in console 
 	}
 
+	public static int basicBranchAndLoops() {
+		Random rand = new Random(); // a random instance 
+		int n = rand.nextInt(50);   // generate a number between 0~49
+		while(n < 100) {
+			System.out.println(n);
+			n = n > 60 ? n + 5 : n * 2; // conditional snetence
+		}
+		
+		for(int i = 0; i < 5; i++) { //same `for` as c and c++
+			if (n > 110 && n < 1000) { /* operator `&&` and `||`,
+			Still obeys the Short Circuit rule */
+				n *= i;
+			}
+			else continue; // `continue` and `break` 
+		}
+		return n;
+	}
 } 
+
+
+
