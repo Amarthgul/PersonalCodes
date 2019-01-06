18e6ad9c147df863c4cb1a98f03d2edf066d7eda
Amarth Gul <amarthgul@gmail.com>
2019-01-06 15:14:56 -0500

    Update colorCube.py

--- Sharing/colorCube.py
+++ Sharing/colorCube.py
@@ -1,34 +1,57 @@
-def colorCube(step = 32):
-    ''' plot a 3D RGB color cube
-step: sample rate between 0 to 255. By default, this value is set to 32 for a moderate amount of calculation
+def colorCube(step = 10, mode = 'rgb'):
+    ''' plot a 3D color cube
+step: sample rate, higher value requires more timr to sample;
+mode: either rgb or hsv;
     '''
     import numpy as np
     import matplotlib.pyplot as plt
+    import matplotlib.colors as cls
     from mpl_toolkits.mplot3d import Axes3D
 
-    start = 0;
-    end = 255;
-    fontSize = 15
+    
     plotSize = 6.5
     pointSize = 30
+    fontSize = 15
 
     plt.figure(figsize = (plotSize, plotSize))
     ax3D = plt.axes([0.05, 0.05, 0.9, 0.9], projection = '3d')
-    for zValue in range(start, end, step):
-        for xValue in range(start, end, step):
-            for yValue in range(start, end, step):
-                RGB = '#%02x%02x%02x' % (xValue, yValue, zValue);
-                ax3D.scatter(xValue, yValue, zValue, c = RGB, s = pointSize)
 
-    ax3D.set_xlabel('$Red Value$', fontsize = fontSize)    
-    ax3D.set_ylabel('$Blue Value$', fontsize = fontSize)    
-    ax3D.set_zlabel('$Green Value$', fontsize = fontSize)  
-      
+    #=======================================================
+    if mode.lower() == 'rgb':
+        start = 0;
+        end = 255;
+
+        for zValue in np.linspace(start, end, step):
+            for xValue in np.linspace(start, end, step):
+                for yValue in np.linspace(start, end, step):
+                    color = '#%02x%02x%02x' % (xValue, yValue, zValue);
+                    ax3D.scatter(xValue, yValue, zValue, c = color, s = pointSize)
+
+        ax3D.set_xlabel('$Red Value$', fontsize = fontSize)    
+        ax3D.set_ylabel('$Green Value$', fontsize = fontSize)    
+        ax3D.set_zlabel('$Blue Value$', fontsize = fontSize)  
+    #=======================================================
+
+    elif mode.lower() == 'hsv':
+        for hue in np.linspace(0, 360, step):
+            for sat in np.linspace(0, 1, step):
+                for val in np.linspace(0, 1, step):
+                    color = cls.hsv_to_rgb([hue / 360, sat, val])
+                    #matplotlib requires hue to be in range 0 to 1
+                    ax3D.scatter(hue, sat, val, c = color, s = pointSize)
+
+        ax3D.set_xlabel('$Hue$', fontsize = fontSize)    
+        ax3D.set_ylabel('$Saturation$', fontsize = fontSize)    
+        ax3D.set_zlabel('$Value$', fontsize = fontSize)
+    #=======================================================
+    else:
+        raise ValueError("Invalid mode")
+
     plt.show()
     
 
 def main():
-    colorCube()
+    colorCube(step = 10, mode = 'hsv')
 
 if __name__ == '__main__':
     main()
