01b1ab9936add628958ea90873a98b020c9d3fb1
Amarth Gul <amarthgul@gmail.com>
2021-02-02 18:03:42 -0500

    Update temp.py

--- quickSort/temp.py
+++ quickSort/temp.py
@@ -1,9 +1,26 @@
-def depthFirstSearch(problem):
-    '''
-    Writing in the provided logic without any API is completely bullshit
-    '''
-    closedset = set()
-    startPoint = problem.getStartState()
-    openset = [startPoint] # openset starts with starting state
-    parents = dict({})
+start = problem.getStartState()
+    
+    nextLayer = []
+    visited = []
+    visited.append(start)
+    nodeWithPath = [(start, [], 0)] 
 
+    while nodeWithPath:
+        print("-------------------------> current mdoes ", nodeWithPath)
+        for (currentNode, path, layer) in nodeWithPath:
+            if problem.isGoalState(currentNode):
+                print("-------------------------> result: ", path)
+                return path
+            
+            for (child, action, cost) in problem.getSuccessors(currentNode):
+                print("-------------------------> get succ ", child, action, cost)
+                if child not in visited:
+                    nextLayer.append((child, path + [action], layer + 1))
+
+            visited.append(nodeWithPath.pop()[0])
+        
+            
+        for (child, action, layer) in nextLayer:
+            print("-------------------------> translate to nextLayer ", child, action, cost)
+            nodeWithPath.append((child, action, layer))
+        nextLayer = []
