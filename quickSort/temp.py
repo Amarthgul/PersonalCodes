6ed5915b44484f3bf874254d8c19bf65d66d37fe
Amarth Gul <amarthgul@gmail.com>
2021-02-02 22:09:05 -0500

    Update temp.py

--- quickSort/temp.py
+++ quickSort/temp.py
@@ -5,26 +5,3 @@
     visited = []
     visited.append(start)
     nodeWithPath = [(start, [], 0)] 
-
-    while nodeWithPath:
-        #print("-------------------------> current layer ", nodeWithPath, len(nodeWithPath))
-        for (currentNode, path, layer) in nodeWithPath:
-            print("-------------------------> current node ", currentNode)
-            if problem.isGoalState(currentNode):
-                print("-------------------------> found it : ")
-                return path
-            
-            for (child, action, cost) in problem.getSuccessors(currentNode):
-                print("-------------------------> ---- get Successors ", child)
-                if child not in visited:
-                    nextLayer.append((child, path + [action], layer + 1))
-
-            visited.append(currentNode)
-            nodeWithPath.remove((currentNode, path, layer))
-            
-        
-            
-        for (child, action, layer) in nextLayer:
-            #print("-------------------------> ---- translate to nextLayer ", child, action, cost)
-            nodeWithPath.append((child, action, layer))
-        nextLayer = []
